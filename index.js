4102691f3e532411feff5dcc94f2c994947fdfa1
dario.olivini <dario.olivini@pblinking.com>
2017-02-02 15:39:16 +0100

    Added options to call
    Added passed options to plugin runner

--- index.js
+++ index.js
@@ -3,53 +3,53 @@ const fileType = require('file-type');
 const isStream = require('is-stream');
 const tarStream = require('tar-stream');
 
-module.exports = () => input => {
-	if (!Buffer.isBuffer(input) && !isStream(input)) {
-		return Promise.reject(new TypeError(`Expected a Buffer or Stream, got ${typeof input}`));
-	}
+module.exports = () => (input, opts) => {
+    if (!Buffer.isBuffer(input) && !isStream(input)) {
+        return Promise.reject(new TypeError(`Expected a Buffer or Stream, got ${typeof input}`));
+    }
 
-	if (Buffer.isBuffer(input) && (!fileType(input) || fileType(input).ext !== 'tar')) {
-		return Promise.resolve([]);
-	}
+    if (Buffer.isBuffer(input) && (!fileType(input) && !opts.legacyTar || fileType(input) && fileType(input).ext !== 'tar')) {
+        return Promise.resolve([]);
+    }
 
-	const extract = tarStream.extract();
-	const files = [];
+    const extract = tarStream.extract();
+    const files = [];
 
-	extract.on('entry', (header, stream, cb) => {
-		const chunk = [];
+    extract.on('entry', (header, stream, cb) => {
+        const chunk = [];
 
-		stream.on('data', data => chunk.push(data));
-		stream.on('end', () => {
-			const file = {
-				data: Buffer.concat(chunk),
-				mode: header.mode,
-				mtime: header.mtime,
-				path: header.name,
-				type: header.type
-			};
+        stream.on('data', data => chunk.push(data));
+        stream.on('end', () => {
+            const file = {
+                data: Buffer.concat(chunk),
+                mode: header.mode,
+                mtime: header.mtime,
+                path: header.name,
+                type: header.type
+            };
 
-			if (header.type === 'symlink' || header.type === 'link') {
-				file.linkname = header.linkname;
-			}
+            if (header.type === 'symlink' || header.type === 'link') {
+                file.linkname = header.linkname;
+            }
 
-			files.push(file);
-			cb();
-		});
-	});
+            files.push(file);
+            cb();
+        });
+    });
 
-	const promise = new Promise((resolve, reject) => {
-		extract.on('finish', () => resolve(files));
-		extract.on('error', reject);
-	});
+    const promise = new Promise((resolve, reject) => {
+        extract.on('finish', () => resolve(files));
+        extract.on('error', reject);
+    });
 
-	extract.then = promise.then.bind(promise);
-	extract.catch = promise.catch.bind(promise);
+    extract.then = promise.then.bind(promise);
+    extract.catch = promise.catch.bind(promise);
 
-	if (Buffer.isBuffer(input)) {
-		extract.end(input);
-	} else {
-		input.pipe(extract);
-	}
+    if (Buffer.isBuffer(input)) {
+        extract.end(input);
+    } else {
+        input.pipe(extract);
+    }
 
-	return extract;
+    return extract;
 };
